9342b083324eeefc6c06a0a93052983ecb371367
Kenza Houben <kenza.tabti@student.ehb.be>
2025-02-27 11:43:39 +0100

    render is done

--- js/main.js
+++ js/main.js
@@ -6,6 +6,15 @@ let adjectives;
 let sortDirection = "up";
 
 function init() {
+    // TODO 1: JSON inladen
+    const adjObject = getAdjectives();
+    // TODO 2: JSON object maken
+    adjectives = JSON.parse(adjObject);
+    console.log(adjectives);
+    // TODO 3: Call render
+    render(adjectives);
+    // TODO 4: Call addSortEvents
+    addSortEvents();
 
 }
 
@@ -22,6 +31,29 @@ function sort() {
 }
 
 function render() {
+    // TODO 3.1: add HTML to HTML string forEach adjective in adjectives
+    // TODO 3.2: add class based on score (>= 6 is 'good')
+    // TODO 3.3: HTML string toevoegen aan container
+    const sjabloon = document.querySelector('.word-list');
+    sjabloon.innerHTML = "";
+    let HTML = "";
+
+    console.log(adjectives);
+
+    adjectives.forEach(function (bv) {
+        HTML += `
+         <div class="word-item">
+            <span class="word-score bad">${bv.score}</span>
+            <span>${bv.word}</span>
+            <div class="vote-buttons">
+                <button value="ok" class="upvote-button">👍</button>
+                <button value="ok" class="downvote-button">👎</button>
+            </div>
+        </div>
+        `
+    });
+    HTML += "</div>"
+    sjabloon.innerHTML = HTML;
 
 }
 
