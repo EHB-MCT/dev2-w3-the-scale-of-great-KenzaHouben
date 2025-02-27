f9b32bb2b44b1450e9667dcc26cb4a235195c8e5
Kenza Houben <kenza.tabti@student.ehb.be>
2025-02-27 12:58:11 +0100

    sort momenteel done

--- js/main.js
+++ js/main.js
@@ -15,11 +15,14 @@ function init() {
     render(adjectives);
     // TODO 4: Call addSortEvents
     addSortEvents(adjectives);
-
+    // TODO 5: Call sort
+    sort(adjectives);
 }
 
 function addSortEvents() {
+    document.querySelector('#sort-up').addEventListener('click', function () {
 
+    })
 }
 
 function addVoteEvents() {
@@ -27,26 +30,49 @@ function addVoteEvents() {
 }
 
 function sort() {
-
+    // TODO: check active sort (default: up)
+    if (sortDirection == "up") {
+        adjectives = adjectives.sort(function (a, b) {
+            // TODO: sort
+            if (a.score < b.score) {
+                return 1
+            } else {
+                return -1
+            }
+        })
+    } else if (sortDirection == "down") {
+        adjectives = adjectives.sort(function (a, b) {
+            // TODO: sort
+            if (a.score > b.score) {
+                return 1
+            } else {
+                return -1
+            }
+        })
+    }
+    // TODO: render
+    render();
 }
 
 function render() {
-    console.log(adjectives);
     // TODO 3.1: add HTML to HTML string forEach adjective in adjectives
-    const sjabloon = document.querySelector('.word-list');
+    const sjabloon = document.querySelector('#container');
     sjabloon.innerHTML = "";
     let HTML = "";
 
-    // TODO 3.2: add class based on score (>= 6 is 'good')
-    let classScore = "";
-
-
-
-
     adjectives.forEach(function (bv) {
+
+        // TODO 3.2: add class based on score (>= 6 is 'good')
+        let classScore = "";
+        if (bv.score >= 6) {
+            classScore = "good";
+        } else {
+            classScore = "bad";
+        }
+
         HTML += `
          <div class="word-item">
-            <span class="word-score bad">${bv.score}</span>
+            <span class="word-score ${classScore}">${bv.score}</span>
             <span>${bv.word}</span>
             <div class="vote-buttons">
                 <button value="ok" class="upvote-button">👍</button>
@@ -55,9 +81,9 @@ function render() {
         </div>
         `
     });
+    // Data in string verwerken
     HTML += "</div>"
     sjabloon.innerHTML = HTML;
-
     // TODO 3.3: HTML string toevoegen aan container
 }
 
